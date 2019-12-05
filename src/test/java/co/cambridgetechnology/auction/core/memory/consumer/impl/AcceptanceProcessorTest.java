b990ef8c260c0facec01969e33345d94e2bc7739
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-12-05 15:26:14 +0800

    add coverage

--- src/test/java/co/cambridgetechnology/auction/core/memory/consumer/impl/AcceptanceProcessorTest.java
+++ src/test/java/co/cambridgetechnology/auction/core/memory/consumer/impl/AcceptanceProcessorTest.java
@@ -5,9 +5,9 @@ import co.cambridgetechnology.auction.core.memory.entity.AuctionItem;
 import co.cambridgetechnology.auction.core.memory.entity.TransactionEvent;
 import co.cambridgetechnology.auction.core.memory.entity.Type;
 import co.cambridgetechnology.auction.core.memory.exception.InvalidBusinessRuleException;
+import co.cambridgetechnology.auction.core.memory.model.request.impl.AcceptRequest;
 import co.cambridgetechnology.auction.core.memory.model.request.impl.BidRequest;
 import co.cambridgetechnology.auction.core.memory.service.AuctionService;
-import co.cambridgetechnology.auction.core.memory.service.BidEntryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,17 +19,15 @@ import java.math.BigDecimal;
 
 import static org.mockito.ArgumentMatchers.nullable;
 
-public class BidProcessorTest {
+public class AcceptanceProcessorTest {
     @Mock
     private ObjectMapper objectMapper;
     @Mock
     private AuctionService auctionService;
     @Mock
-    private BidEntryService bidEntryService;
-    @Mock
     private Producer producer;
     @InjectMocks
-    private BidProcessor bidProcessor;
+    private AcceptanceProcessor acceptanceProcessor;
 
     @Before
     public void before() {
@@ -38,46 +36,44 @@ public class BidProcessorTest {
 
     @Test
     public void testProcess()throws IOException {
-        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(BidRequest.class))).thenReturn(new BidRequest("vase123","johnny", BigDecimal.TEN));
-        Mockito.when(auctionService.bid(nullable(BidRequest.class))).thenReturn(new AuctionItem("vase123","andrew"));
+        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(AcceptRequest.class))).thenReturn(new AcceptRequest());
+        Mockito.when(auctionService.accept(nullable(AcceptRequest.class))).thenReturn(new AuctionItem("vase123","andrew"));
         TransactionEvent transactionEvent = new TransactionEvent();
         transactionEvent.setId("abcd1234");
         transactionEvent.setRequest("{}");
-        transactionEvent.setType(Type.BID.toString());
-        bidProcessor.process(transactionEvent);
+        transactionEvent.setType(Type.ACCEPTANCE.toString());
+        acceptanceProcessor.process(transactionEvent);
         Mockito.verify(producer).produceSuccess(nullable(String.class), nullable(Object.class), nullable(Integer.class), nullable(long.class));
         Mockito.verify(producer, Mockito.never()).produceError(nullable(String.class), nullable(InvalidBusinessRuleException.class), nullable(Integer.class), nullable(long.class));
     }
 
     @Test
-    public void testProcess_cannotRead() throws IOException {
-        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(BidRequest.class)))
-                .thenThrow(new IOException());
-        Mockito.when(auctionService.bid(nullable(BidRequest.class))).thenReturn(new AuctionItem("vase123","andrew"));
+    public void testProcess_cannotRead()throws IOException {
+        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(AcceptRequest.class))).thenThrow(new IOException());
+        Mockito.when(auctionService.accept(nullable(AcceptRequest.class))).thenReturn(new AuctionItem("vase123","andrew"));
         TransactionEvent transactionEvent = new TransactionEvent();
         transactionEvent.setId("abcd1234");
         transactionEvent.setRequest("{}");
-        transactionEvent.setType(Type.BID.toString());
-        bidProcessor.process(transactionEvent);
+        transactionEvent.setType(Type.ACCEPTANCE.toString());
+        acceptanceProcessor.process(transactionEvent);
         Mockito.verify(producer, Mockito.never()).produceSuccess(nullable(String.class), nullable(Object.class), nullable(Integer.class), nullable(long.class));
         Mockito.verify(producer).produceError(nullable(String.class), nullable(InvalidBusinessRuleException.class), nullable(Integer.class), nullable(long.class));
     }
 
     @Test
-    public void testProcess_serviceFail() throws IOException {
-        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(BidRequest.class)))
-                .thenReturn(new BidRequest("vase123","johnny", BigDecimal.TEN));
-        Mockito.when(auctionService.bid(nullable(BidRequest.class))).thenThrow(new InvalidBusinessRuleException("service fail"));
+    public void testProcess_serviceFail()throws IOException {
+        Mockito.when(objectMapper.readValue(nullable(String.class), ArgumentMatchers.eq(AcceptRequest.class))).thenReturn(new AcceptRequest());
+        Mockito.when(auctionService.accept(nullable(AcceptRequest.class))).thenThrow(new InvalidBusinessRuleException("service fail"));
         TransactionEvent transactionEvent = new TransactionEvent();
         transactionEvent.setId("abcd1234");
         transactionEvent.setRequest("{}");
-        transactionEvent.setType(Type.BID.toString());
-        bidProcessor.process(transactionEvent);
+        transactionEvent.setType(Type.ACCEPTANCE.toString());
+        acceptanceProcessor.process(transactionEvent);
         Mockito.verify(producer, Mockito.never()).produceSuccess(nullable(String.class), nullable(Object.class), nullable(Integer.class), nullable(long.class));
         Mockito.verify(producer).produceError(nullable(String.class), nullable(InvalidBusinessRuleException.class), nullable(Integer.class), nullable(long.class));
     }
     @Test
     public void testGetType() {
-        Assert.assertEquals(Type.BID.toString(), bidProcessor.getType());
+        Assert.assertEquals(Type.ACCEPTANCE.toString(), acceptanceProcessor.getType());
     }
 }
